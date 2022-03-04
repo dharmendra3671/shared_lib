fc4071053ff66e4e2e845b21323c1bcdfefabed6
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-04 13:08:39 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,9 @@
-// def add(x,y){
-//   echo "Sum of ${x} and ${y} is ${x+y}"
-// }
-// def mul(x,y){
-//   echo "mul of ${x} and ${y} is ${x*y}"
-// }
-// def div(x,y){
-//   echo "mul of ${x} and ${y} is ${x/y}"
-// }
-def artifact(url,username,password):
-    println("USERNAME is:${user} and PASSWORD is:${password}");
-
-// artifact('http://127.0.0.1:8082/artifactory/Zipfile_Artifactory/',
-//             'admin','Kumar@6805'
-//         )
+def add(x,y){
+  echo "Sum of ${x} and ${y} is ${x+y}"
+}
+def mul(x,y){
+  echo "mul of ${x} and ${y} is ${x*y}"
+}
+def div(x,y){
+  echo "mul of ${x} and ${y} is ${x/y}"
+}
