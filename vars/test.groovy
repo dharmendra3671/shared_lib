9e3025356f571c41eb7cecf6a94662c5b017eae8
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-06 23:03:49 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,3 @@
-def add(x,y){
-  echo "Sum of ${x} and ${y} is ${x+y}"
-}
-def mul(x,y){
-  echo "mul of ${x} and ${y} is ${x*y}"
-}
-def div(x,y){
-  echo "mul of ${x} and ${y} is ${x/y}"
+def uloadfile(){
+  bat "jf_art"
 }
